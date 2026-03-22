c46a52d104d1514034d6537c99c5da6a0108fd40
Andrii Voloshyn <25062006vas@gmail.com>
2026-03-22 12:30:04 +0200

    Create last function and main

--- lab2.cpp
+++ lab2.cpp
@@ -80,3 +80,95 @@ double test_mutex(const vector<int> &arr, int threads_num)
     auto end = chrono::high_resolution_clock::now();
     return chrono::duration<double, milli>(end - start).count();
 }
+
+void worker_cas(const vector<int> &arr, int start_idx, int end_idx)
+{
+    for (int i = start_idx; i < end_idx; i++)
+    {
+        if (arr[i] % 3 == 0)
+        {
+            int expected_c;
+            do
+            {
+                expected_c = total_count_cas.load();
+            } while (!total_count_cas.compare_exchange_weak(expected_c, expected_c + 1));
+
+            int expected_m;
+            do
+            {
+                expected_m = max_val_cas.load();
+                if (arr[i] <= expected_m)
+                {
+                    break;
+                }
+            } while (!max_val_cas.compare_exchange_weak(expected_m, arr[i]));
+        }
+    }
+}
+
+double test_cas(const vector<int> &arr, int threads_num)
+{
+    total_count_cas = 0;
+    max_val_cas = -1;
+    vector<thread> ths;
+    int chunk = arr.size() / threads_num;
+
+    auto start = chrono::high_resolution_clock::now();
+
+    for (int i = 0; i < threads_num; i++)
+    {
+        int start_idx = i * chunk;
+        int end_idx = (i == threads_num - 1) ? arr.size() : (i + 1) * chunk;
+        ths.push_back(thread(worker_cas, ref(arr), start_idx, end_idx));
+    }
+
+    for (int i = 0; i < threads_num; i++)
+    {
+        ths[i].join();
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    return chrono::duration<double, milli>(end - start).count();
+}
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int threads_num = 4;
+    vector<int> sizes = {100000, 1000000, 5000000, 10000000};
+
+    cout << "=== Тестування на " << threads_num << " потоках ===\n\n";
+
+    for (int size : sizes)
+    {
+        vector<int> arr(size);
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = rand() % 100000 + 1;
+        }
+
+        double time_seq = test_seq(arr);
+        double time_mutex = test_mutex(arr, threads_num);
+        double time_cas = test_cas(arr, threads_num);
+
+        cout << "Розмір масиву: " << size << " елементів\n";
+
+        cout << "[Послідовно] Кількість: " << total_count_seq
+             << ", Найбільше: " << max_val_seq
+             << " | Час: " << time_seq << " мс\n";
+
+        cout << "[М'ютекс]    Кількість: " << total_count_mutex
+             << ", Найбільше: " << max_val_mutex
+             << " | Час: " << time_mutex << " мс\n";
+
+        cout << "[CAS]        Кількість: " << total_count_cas.load()
+             << ", Найбільше: " << max_val_cas.load()
+             << " | Час: " << time_cas << " мс\n";
+
+        cout << "------------------------------------------------------------------\n";
+    }
+
+    return 0;
+}
